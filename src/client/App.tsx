c02a57e4965eb58fb4ca85782f8e8ebb18933357
Anya Aven <leafeon96@gmail.com>
2024-06-11 17:37:37 -0400

    Added test for client to server connection

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { basename } from "path";
+
+const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL || "http://localhost:3000/";
 
 export default function App() {
+  const [test, setTest] = useState({})
 
+  useEffect(
+    function getTest(){
+
+      async function fetchTest(){
+        const resp = await fetch(BASE_URL)
+        const data: {KEY: string} = await resp.json()
+
+        setTest(data);
+      }
+
+      fetchTest();
+    }, [])
 
   return (
     <BrowserRouter>
@@ -9,7 +26,9 @@ export default function App() {
         <Route path="/" element={
           <div>
             first page
-          </div>} />
+            { "KEY" in test && <div>test.KEY</div>}
+          </div>}
+          />
       </Routes>
     </BrowserRouter>
   );
