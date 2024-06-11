106567f86cf248aa290c85b6b6393a8f13d463e4
Anya Aven <leafeon96@gmail.com>
2024-06-11 17:20:02 -0400

    Added errors and router

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,8 @@
-
+import { NotFoundError } from "./expressError.js";
+import { Router } from "express";
 import express from "express";
 import ViteExpress from "vite-express";
 import bodyParser from "body-parser"
-
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,5 +15,31 @@ const server = app.listen(port, () => {
 });
 
 ViteExpress.bind(app, server);
+
+const router: Router = new (Router as any)(); // <-- TODO Research later a better solve
+
+/** TEST HOME */
+app.get("/", async function (req, res, next) {
+
+  return res.json({ "KEY": "VALUE" });
+});
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  throw new NotFoundError();
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err: any, req: any, res: any, next: any) { // <-- TODO add typing
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  /* istanbul ignore next (ignore for coverage) */
+  const status= err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
 
 export { app };
