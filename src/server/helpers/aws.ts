325964d80092913e92a4ca56e388ce9cc36cdc4a
Anya Aven <leafeon96@gmail.com>
2024-06-12 18:00:37 -0400

    Added helper command for putObjectInBucket

---
+++ src/server/helpers/aws.ts
@@ -0,0 +1,30 @@
+/** Helper commands to interact with s3 AWS */
+import { tUploadParamsAWS } from "@/shared/types.js";
+import { removeFile } from "./metadata.js";
+
+import {
+  S3Client,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { BadRequestError } from "expressError.js";
+
+/** AWS s3 client for bucket */
+const s3Client = new S3Client(
+  {
+    region: process.env.AWS_REGION || "WRONG REGION",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY || 'WRONG KEY',
+      secretAccessKey: process.env.AWS_SECRET || 'WRONG SECRET'
+    }
+  }
+);
+
+export async function putObjectInBucket(uploadObject: tUploadParamsAWS){
+  try {
+    const data = await s3Client.send(new PutObjectCommand(uploadObject));
+    console.log('Success put in AWS:', data);
+    return data
+  } catch (err: any) {
+    throw new BadRequestError("Could not put object in AWS Bucket");
+  };
+}
