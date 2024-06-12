4c062d0b27026d6e85b34ad3a1e63c1c67ee3703
Anya Aven <leafeon96@gmail.com>
2024-06-12 00:04:14 -0400

    Created simple form for putting objects into AWS Bucket

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,21 +4,21 @@ import { useState, useEffect } from "react";
 const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL || "http://localhost:3001/";
 
 export default function App() {
-  const [test, setTest] = useState <Record<string, string>>({})
+  const [test, setTest] = useState<Record<string, string>>({});
 
-  useEffect(
-    function getTest(){
+  // useEffect(
+  //   function getTest() {
 
-      async function fetchTest(){
-        const resp = await fetch(BASE_URL)
-        const data: {KEY: string} = await resp.json()
+  //     async function fetchTest() {
+  //       const resp = await fetch(BASE_URL);
+  //       const data: { KEY: string; } = await resp.json();
 
-        setTest(data);
-      }
+  //       setTest(data);
+  //     }
 
-      fetchTest();
-    }, []
-  )
+  //     fetchTest();
+  //   }, []
+  // );
 
   return (
     <BrowserRouter>
@@ -26,9 +26,18 @@ export default function App() {
         <Route path="/" element={
           <div>
             first page
-            { "KEY" in test && <div>{test.KEY}</div>}
-          </div>}
-          />
+
+            <form action={`${BASE_URL}upload`} encType="multipart/form-data" method="post">
+              <div className="form-group">
+                <input type="file" className="form-control-file" name="uploaded_file" />
+                <input type="text" className="form-control" placeholder="Number of speakers" name="nspeakers" />
+                <input type="submit" value="Get me the stats!" className="btn btn-default" />
+              </div>
+            </form>
+
+          </div>
+        }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/server/main.ts
+++ src/server/main.ts
@@ -9,9 +9,15 @@ import bodyParser from "body-parser";
 
 import {
   S3Client,
-  ListObjectsCommand,
+  PutObjectCommand,
 } from "@aws-sdk/client-s3";
 
+import multer from "multer"
+import fs from 'fs';
+
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME || "WRONG BUCKET NAME"
+
+/** AWS s3 client for bucket */
 const s3Client = new S3Client(
   {
     region: "us-east-1",
@@ -22,17 +28,18 @@ const s3Client = new S3Client(
   }
 );
 
-/** ATTEMPT */
-async function listObjectsInBucket(bucketName: string) {
-
-  try {
-    const data = await s3Client.send(new ListObjectsCommand({ Bucket: bucketName }));
-    console.log("Success", data);
+/** PUT OBJECTS IN BUCKET */
+async function putObjectInBucket(bucketName: string, Key: string){
+  // try {
+    const data = await s3Client.send(new PutObjectCommand(
+      { Bucket: bucketName, Key, Body: "heres the body" }));
+    console.log("Success PUT", data);
     return data;
-  } catch (err) {
-    console.log("Error", err);
-    return err;
-  }
+
+  // } catch (err) {
+  //   console.log("Error", err);
+  //   return err;
+  // }
 }
 
 
@@ -50,10 +57,77 @@ ViteExpress.bind(app, server);
 
 const router: Router = new (Router as any)(); // <-- TODO Research later a better solve
 
+
+
 /** TEST HOME */
 router.get("/", async function (req, res, next) {
-  //const data = await listObjectsInBucket("pixly-anya");
-  return res.json({ "KEY": "VALUE" });
+  //const resp = await putObjectInBucket(AWS_BUCKET_NAME, "new-key");
+  // const respStr = await resp.ContentLength
+  // console.log({respStr})
+
+  return res.json({ "KEY": "HOME VALUE" });
+});
+
+const upload = multer({ dest: 'uploads/' });
+
+//https://github.com/expressjs/multer
+/** TEST ROUTE TO UPLOAD AN IMG
+ *
+ * Middleware: upload.single("NAME FIELD"):
+ * It is important that you use the name field value from the form in your upload function.
+ * This tells multer which field on the request it should look for the files in.
+ * If these fields aren't the same in the HTML form and on your server, your upload will fail:
+*/
+router.post('/upload', upload.single('uploaded_file'), async (req, res) => {
+  console.log("UPLOAD ROUTE");
+  if (!req.file) {
+    return res.status(400).send({ error: 'No file uploaded' });
+  }
+  // req.body will hold the text fields, if there were any
+
+  const file = req.file;
+  const filePath = file.path;
+
+  //https://www.geeksforgeeks.org/node-js-fs-createreadstream-method/
+  //https://www.geeksforgeeks.org/difference-between-readfile-and-createreadstream-in-node-js/
+  const fileStream = fs.createReadStream(filePath);
+
+  const uploadParams = {
+    Bucket: AWS_BUCKET_NAME,
+    Key: `${file.originalname}`, //TODO: PUT AS A KEY FROM DB
+    Body: fileStream,
+    ContentType: file.mimetype,
+  };
+
+  console.log({uploadParams})
+  try {
+    const data = await s3Client.send(new PutObjectCommand(uploadParams));
+    console.log('Success PUT', data);
+    res.status(200).send({ message: 'File uploaded successfully', data });
+
+    // Delete the temporary file
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Error deleting temporary file:', err);
+      } else {
+        console.log('Temporary file deleted');
+      }
+    });
+  } catch (err: any) {
+    console.error('Error uploading file:', err);
+    res.status(500).send({ error: err.message });
+
+    // Delete the temporary file in case of error
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Error deleting temporary file:', err);
+      } else {
+        console.log('Temporary file deleted');
+      }
+    });
+  }
+
+  return res.json({success: uploadParams})
 });
 
 app.use("/", router);
@@ -66,7 +140,6 @@ app.use(function (req, res, next) {
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err: any, req: any, res: any, next: any) { // <-- TODO add typing
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  /* istanbul ignore next (ignore for coverage) */
   const status = err.status || 500;
   const message = err.message;
 
