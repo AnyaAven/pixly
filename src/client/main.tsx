cf0ce225b385ee7ceaa1e986c83c21b5b1758007
Anya Aven <leafeon96@gmail.com>
2024-06-12 22:48:05 -0400

    Removed strict mode for no double console logs

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,11 +2,9 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App.js";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 );
