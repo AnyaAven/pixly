42f24988efb5df87bcfb8cea5d90b250da4c0cd8
Anya Aven <leafeon96@gmail.com>
2024-06-12 22:48:26 -0400

    Added nav

---
+++ src/client/NavBar.tsx
@@ -0,0 +1,29 @@
+import { Link, NavLink } from "react-router-dom";
+import { Nav, NavItem } from "reactstrap";
+import "./NavBar.css";
+
+/** Top navigation bar for site. */
+
+function NavBar() {
+  console.log("* NavBar");
+
+  return (
+    <nav className="NavBar navbar navbar-expand-md">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Pixly
+        </Link>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink to="/images">images</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/images/upload">Upload!</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
