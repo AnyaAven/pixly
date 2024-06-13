e12d93855de3fef32765b4a46e1e8b0b156df19d
Anya Aven <leafeon96@gmail.com>
2024-06-12 22:46:11 -0400

    Added model and upload types for Image

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,17 @@
 import fs from "fs"
 
+/** For database model */
+export type tImageModel = {
+  id: number,
+  filename: string,
+  height: number,
+  width: number,
+  orientation: "landscape" | "portrait",
+  isEditted: boolean,
+  description?: string,
+  comment?: string,
+}
+
 /** Useful for PutObjectCommand from "@aws-sdk/client-s3" */
 export type tUploadParamsAWS = {
   Bucket: string;
@@ -7,3 +19,17 @@ export type tUploadParamsAWS = {
   Body: fs.ReadStream;
   ContentType: string;
 }
+
+/** For uploading an Image in App */
+export type tImageUpload = {
+  uploadedFile: null | File;
+  filename: string;
+  description: string;
+  comment: string;
+  orientation: "landscape" | "portrait";
+}
+
+/** Generic HTTP methods */
+export type tHTTPMethods =
+"GET" |
+"POST"
