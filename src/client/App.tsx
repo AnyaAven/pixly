87a84e9078f1803aa4cff220b80ee1a3555f371e
Anya Aven <leafeon96@gmail.com>
2024-06-12 22:53:04 -0400

    Finished on mount and uploadImage functionality

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,33 +1,90 @@
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Loader from "Loader";
+import NavBar from "NavBar";
+import ImageUploadForm from "forms/ImageUploadForm";
 
-const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL || "http://localhost:3001/";
+const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL || "http://localhost:3001";
+import { tImageModel, tImageUpload } from "@/shared/types";
+
+import PixlyApi from "api/pixlyApi";
 
 export default function App() {
-  const [test, setTest] = useState<Record<string, string>>({});
+  console.log("App");
 
-  // useEffect(
-  //   function getTest() {
+  const [images, setImages] = useState<tImageModel[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [errors, setErrors] = useState<null | []>([]); // TODO think about the type here
 
-  //     async function fetchTest() {
-  //       const resp = await fetch(BASE_URL);
-  //       const data: { KEY: string; } = await resp.json();
+  /** Fetch all images on mount */
+  useEffect(
+    function fetchImagesOnMount() {
+      console.log("fetchImagesOnMount");
 
-  //       setTest(data);
-  //     }
+      async function getImages() {
+        const resp = await fetch(`${BASE_URL}/images/`);
+        const data: tImageModel[] = await resp.json();
 
-  //     fetchTest();
-  //   }, []
-  // );
+        setImages(data);
+        setIsLoading(false);
+        console.log("Fetched:", { data });
+      }
+
+      if (isLoading) {
+        getImages();
+      }
+    }, []);
+
+  async function uploadImage(formData: tImageUpload) {
+    console.log("APP: uploadImage");
+
+    // let image: tImageModel;
+    // try {
+    //   image = await PixlyApi.uploadImage(formData);
+    // } catch (err) {
+    //   setErrors(err as [])
+    //   return;
+    // }
+    //TODO: move to PixlyApi and handle sending in formdata and not just json
+    const data = new FormData();
+    data.append("uploadedFile", formData.uploadedFile!);
+    data.append("filename", formData.filename);
+    data.append("description", formData.description);
+    data.append("comment", formData.comment);
+    data.append("orientation", formData.orientation);
+
+    try {
+      const response = await fetch("http://localhost:3001/images/upload", {
+        method: "POST",
+        body: data,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const image: tImageModel = await response.json();
+      console.log("APP: uploadImage", { image });
+      setImages((currImages) => [...currImages, image]);
+    } catch (err) {
+      console.error("Failed to upload image", err);
+      setErrors(err as []);
+    }
+  }
+
+  if (isLoading) return <Loader />;
 
   return (
     <BrowserRouter>
+
+      <NavBar></NavBar>
+
       <Routes>
         <Route path="/" element={
           <div>
             first page
-
-            <form action={`${BASE_URL}upload`} encType="multipart/form-data" method="post">
+            <form action={`${BASE_URL}/upload`} encType="multipart/form-data" method="post">
               <div className="form-group">
                 <input type="file" className="form-control-file" name="uploaded_file" />
                 <input type="text" className="form-control" placeholder="Number of speakers" name="nspeakers" />
@@ -37,6 +94,10 @@ export default function App() {
 
           </div>
         }
+        />
+
+        <Route path="/images/upload"
+          element={<ImageUploadForm uploadImage={uploadImage} />}
         />
       </Routes>
     </BrowserRouter>
